eee3a7895ee614315a21f9f4c9c7fdf31827b75e
s1kko1337 <r.melo.kk@gmail.com>
2025-06-18 13:18:41 +0300

    add some

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
@@ -25,5 +27,32 @@ class RoleSeeder extends Seeder
         $studentRole->givePermissionTo($basicPermission);
         $curatorRole->givePermissionTo($basicPermission,$curatorPermission);
         $adminRole->givePermissionTo([$basicPermission, $curatorPermission, $adminPermission]);
+
+        // Создаем или обновляем пользователя
+        $user = User::firstOrCreate(
+            ['email' => 'sikko4890@gmail.com'],
+            [
+                'name' => 'Admin',
+                'surname' => 'User',
+                'password' => Hash::make('sikko4890@gmail.com'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $role = Role::findByName('curator');
+        $user->assignRole('curator');
+
+        $user->tokens()->delete();
+        $permissions = $user->getPermissionsViaRoles()->pluck('name')->toArray();
+
+        $token = $user->createToken(
+            name: "Seeder Token for {$user->email}",
+            abilities: $permissions
+        )->plainTextToken;
+
+        $this->command->info("User created:");
+        $this->command->line("Email: sikko4890@gmail.com");
+        $this->command->line("Password: sikko4890@gmail.com");
+        $this->command->line("Token: $token");
     }
 }
